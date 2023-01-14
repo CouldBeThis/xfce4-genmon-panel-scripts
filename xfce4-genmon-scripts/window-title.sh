13a180e600d29a68519c77e43cd27cd432449711
CouldBeThis <53547181+CouldBeThis@users.noreply.github.com>
2023-01-14 10:17:21 -0500

    add line breaks

--- xfce4-genmon-scripts/window-title.sh
+++ xfce4-genmon-scripts/window-title.sh
@@ -7,7 +7,7 @@ NUMBER_OF_OPENED_WINDOWS=$(wmctrl -l | wc -l)
 LENGTH_OF_TITLE=$(echo ${WINDOW_TITLE})
 
 INFO="<txt>"
-INFO+="<span weight='Regular' fgcolor='White'>" 
+INFO+="<span weight='Regular' fgcolor='grey'>"
 INFO+="${WINDOW_TITLE}"
 if [ "${LENGTH_OF_TITLE}" -eq 65 ]; then
     INFO+="..."
@@ -15,7 +15,13 @@ fi
 INFO+="</span>"
 INFO+="</txt>"
 if [ "${NUMBER_OF_OPENED_WINDOWS}" -gt 3 ]; then #find the optimum number of opened up windows at startup. I've set 3 since its XFCE4-Panel, Plank and Desktop. 
- INFO+="<txtclick>rofi -combi-modi window -theme slate -show window -icon-theme "Papirus" -show-icons</txtclick>"
+INFO+="<txtclick>
+	rofi -combi-modi window
+	-theme sidetab
+	-show window
+	-icon-theme "Papirus"
+	-show-icons
+</txtclick>"
 fi
 echo -e "${INFO}"
 
